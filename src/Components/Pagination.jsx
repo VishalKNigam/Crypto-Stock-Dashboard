3c85681e4628839180b0206509008f0e95801633
Vishal Kumar Nigam <115461865+VishalKNigam@users.noreply.github.com>
2024-03-09 22:39:42 +0530

    Pagination added Successfully

--- src/Components/Pagination.jsx
+++ src/Components/Pagination.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import { Button, Flex } from "@chakra-ui/react";
+import React from "react";
+import { useSelector } from "react-redux";
 
-const Pagination = () => {
+export const Pagination = ({ page,setPage }) => {
+  const coins = useSelector((store) => store.appReducer.coins);
   return (
-    <div>Pagination</div>
-  )
-}
-
-export default Pagination
+    <>
+      {coins.length > 0 ? (
+        <Flex gap="1rem" alignItems={"center"} justifyContent={"center"}>
+          <Button
+            size={"sm"}
+            variant={1 == page ? "disabled" : "outline"}
+            onClick={() => setPage(page - 1)}
+          >
+            Prev
+          </Button>
+          <Button
+            size={"sm"}
+            variant={"outline"}
+            onClick={() => setPage(page + 1)}
+          >
+            Next
+          </Button>
+        </Flex>
+      ) : (
+        <></>
+      )}
+    </>
+  );
+};
